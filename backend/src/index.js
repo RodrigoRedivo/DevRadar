4b94817eec2af3d6b1150a98dc9d833dfe7e88c6
RodrigoRedivo <rodrigo.a.redivo@gmail.com>
2020-01-19 11:08:28 -0300

    add backend/function advanced

---
+++ backend/src/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const http = require('http');
+const routes = require('./routes');
+const { setupWebSocket } = require('./websocket');
+
+const app = express();
+const server = http.Server(app)
+
+setupWebSocket(server);
+
+mongoose.connect('mongodb+srv://rodrigoredivo:Realmadrid101907@oministack-qmjha.mongodb.net/week10?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+});
+
+app.use(cors({ origin: 'http://localhost:3000' }));
+app.use(express.json());
+app.use(routes);
+
+server.listen(3333);
